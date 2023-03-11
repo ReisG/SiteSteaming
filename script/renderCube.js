a02455c0f616f9413efb1caa642d992bf8187b3d
ReisG <romanryabcev2006@gmail.com>
2023-03-11 12:34:52 +0700

    second try

--- script/renderCube.js
+++ script/renderCube.js
@@ -22,7 +22,7 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 12;
+camera.position.z = 5;
 
 function animate()
 {
