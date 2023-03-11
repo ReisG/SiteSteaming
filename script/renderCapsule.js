645af8478acc0b0e0bad3ed525977ef5701ae73a
ReisG <romanryabcev2006@gmail.com>
2023-03-11 16:23:22 +0700

    moved back to cube

--- script/renderCapsule.js
+++ script/renderCapsule.js
@@ -26,7 +26,7 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const capsule = new THREE.Mesh(geometry, material);
 scene.add(capsule);
 */
-const geometry = new THREE.SphereGeometry( 15, 32, 16 );
+const geometry = THREE.BoxGeometry(1, 1, 1);/*new THREE.SphereGeometry( 15, 32, 16 );*/
 const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 const sphere = new THREE.Mesh( geometry, material );
 scene.add( sphere );
